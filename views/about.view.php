3d6fc6033693af5abe724eca9566d7b9d201a6b7
paszosky <pavel.kowalczyk@gmail.com>
2025-02-06 23:41:52 +0100

    partials views

---
+++ views/about.view.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl" class="h-full bg-gray-100">
+<head>
+    <meta charset="UTF-8">
+    <title>BiegApp</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full">
+    
+    <div class="min-h-full">
+    
+    <?php require('partials/nav.php') ?>
+
+    <header class="bg-white shadow-sm">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">BiegApp about</h1>
+        </div>
+    </header>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <p>Hello to BiegApp. Page About</p>
+        </div>
+    </main>
+    </div>
+
+</body>
+</html>
