328ba6d6a39a1072be164a1018d8f1a3f8536cc2
paszosky <pavel.kowalczyk@gmail.com>
2025-02-06 23:55:30 +0100

    refactor views 3

--- views/about.view.php
+++ views/about.view.php
@@ -1,16 +1,11 @@
 <?php require('partials/head.php'); ?>
-    
-    <?php require('partials/nav.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
 
-    <header class="bg-white shadow-sm">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">BiegApp about</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>Hello to BiegApp. Page About</p>
-        </div>
-    </main>
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>Hello to BiegApp. Page About</p>
+    </div>
+</main>
 
 <?php require('partials/footer.php'); ?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,17 +1,11 @@
-
 <?php require('partials/head.php'); ?>
-    <?php require('partials/nav.php'); ?>
+<?php require('partials/nav.php'); ?>
+<?php require('partials/banner.php') ?>
     
-
-    <header class="bg-white shadow-sm">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>Hello to BiegApp</p>
-        </div>
-    </main>
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <p>Hello to BiegApp</p>
+    </div>
+</main>
 
 <?php require('partials/footer.php'); ?>
