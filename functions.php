34c15f52b7d058b23b6111b3233cedcca5a14b06
paszosky <pavel.kowalczyk@gmail.com>
2025-02-07 23:52:05 +0100

    dynamic menu

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function dd($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '<pre>';
+
+    die();
+}
+
+
+function urlIs($value) {
+    $_SERVER['REQUEST_URI'] === $value;
+}
+
+?>
