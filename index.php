18b30e6733bc1d26dd1ca3fb72adb2771d364cfe
paszosky <pavel.kowalczyk@gmail.com>
2025-02-08 00:26:51 +0100

    router 1

--- index.php
+++ index.php
@@ -8,5 +8,9 @@ $uri = $_SERVER['REQUEST_URI'];
 
 if ($uri === '/biegapp/') {
     require 'controllers/index.php';
+} else if ($uri === '/biegapp/about') {
+    require 'controllers/about.php';
+} else if ($uri === '/biegapp/contact') {
+    require 'controllers/contact.php';
 }
 
