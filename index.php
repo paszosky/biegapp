db2d2b050084485c87edbb97b88269c409a5e86f
paszosky <pavel.kowalczyk@gmail.com>
2025-02-08 00:59:07 +0100

    router 2

--- index.php
+++ index.php
@@ -2,15 +2,8 @@
 
 require 'functions.php';
 
+require 'router.php';
 // dd($_SERVER);
 
-$uri = $_SERVER['REQUEST_URI'];
 
-if ($uri === '/biegapp/') {
-    require 'controllers/index.php';
-} else if ($uri === '/biegapp/about') {
-    require 'controllers/about.php';
-} else if ($uri === '/biegapp/contact') {
-    require 'controllers/contact.php';
-}
-
+    
