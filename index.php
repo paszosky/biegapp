e9eefcaa235eed237b4edd8b097bbc1e3828f530
paszosky <pavel.kowalczyk@gmail.com>
2025-02-08 23:47:01 +0100

    pdo db connection

--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
 // dd($_SERVER);
 
+// connect to our database
 
-    
+$dsn = "mysql:host=localhost;port=3307;dbname=biegapp;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
