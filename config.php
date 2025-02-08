df1618fd9654f6b4c7d79239e9596e72aaa347e1
paszosky <pavel.kowalczyk@gmail.com>
2025-02-09 00:23:47 +0100

    environments configurations

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3307,
+        'dbname' => 'biegapp',
+        'charset' => 'utf8mb4'
+    ]
+];
+
+?>
--- index.php
+++ index.php
@@ -6,9 +6,9 @@ require 'Database.php';
 // require 'router.php';
 // dd($_SERVER);
 
+$config = require('config.php');
 
-
-$db = new Database();
+$db = new Database($config['database']);
 
 $posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
