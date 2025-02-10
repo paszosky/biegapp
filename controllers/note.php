76f277be62b7399d63d3f57b82ba007dbd621ff7
paszosky <pavel.kowalczyk@gmail.com>
2025-02-10 23:37:28 +0100

    authorization

--- controllers/note.php
+++ controllers/note.php
@@ -4,12 +4,20 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Note';
+$currentUserId = 1;
 
-
-$note = $db->query('select * from notes where user_id = :user and id = :id', [
-    'user' => 1,
+$note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
     ])->fetch();
+
+if (! $note) {
+    abort();
+}
+
+
+if ($note['user_id'] != $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 // dd($notes);
 
