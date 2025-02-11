f309ed0e685294ffacc9060bdf26ac6421e8638a
paszosky <pavel.kowalczyk@gmail.com>
2025-02-11 23:06:35 +0100

    styling form page

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,4 +2,8 @@
 
 $heading = "Create note";
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    dd($_POST);
+}
+
 require 'views/note-create.view.php';
--- views/note-create.view.php
+++ views/note-create.view.php
@@ -4,7 +4,34 @@
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1>Create a note</h1>
+
+        <form method="POST">
+            <div class="space-y-12">
+                <div class="border-b border-gray-900/10 pb-12">
+                <h2 class="text-base/7 font-semibold text-gray-900">Create a note</h2>
+                
+
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    
+                    <div class="col-span-full">
+                    <label for="about" class="block text-sm/6 font-medium text-gray-900">Treść notatki</label>
+                        <div class="mt-2">
+                            <textarea name="about" id="about" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                        </div>
+                    </div>
+
+                </div>
+
+                </div>
+
+            </div>
+
+            <div class="mt-6 flex items-center justify-end gap-x-6">
+                
+                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            </div>
+            </form>
+
     </div>
 </main>
 
