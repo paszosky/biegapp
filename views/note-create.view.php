19e4b7fe2fb1cdc983c964f7c79653e2ce1409d4
paszosky <pavel.kowalczyk@gmail.com>
2025-02-12 22:41:54 +0100

    validation form

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -16,7 +16,13 @@
                     <div class="col-span-full">
                     <label for="body" class="block text-sm/6 font-medium text-gray-900">Treść notatki</label>
                         <div class="mt-2">
-                            <textarea name="body" id="body" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"></textarea>
+                            <textarea name="body" id="body" required rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+                                <?= $_POST['body'] ?? '' ?>
+                            </textarea>
+                        
+                            <?php if (isset($errors['body'])) : ?>
+                                <p class="text-red-500 mt-2"><?= $errors['body'] ?></p>
+                            <?php endif; ?>
                         </div>
                     </div>
 
