018816063f4f85090ab7c9b7d7d8661a21469293
paszosky <pavel.kowalczyk@gmail.com>
2025-02-18 15:40:42 +0100

    delete note

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,15 +7,35 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id']
-    ])->findOrFail();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // form was submitted. delete the note
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+        ])->findOrFail();
 
 
-authorize($note['user_id'] == $currentUserId);
+    authorize($note['user_id'] == $currentUserId);
+
+    $db->query('DELETE FROM notes WHERE id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+
+} else {
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+        ])->findOrFail();
 
 
-view("notes/show.view.php", [
-    'heading' => 'Note',
-    'note' => $note
-]);
+    authorize($note['user_id'] == $currentUserId);
+
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
